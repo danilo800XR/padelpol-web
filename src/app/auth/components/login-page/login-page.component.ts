7927a4809dcff1d5c17633acbca38a1965602312
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-16 12:23:48 +0100

    sessionStorage added

--- src/app/auth/components/login-page/login-page.component.ts
+++ src/app/auth/components/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
+import { SessionStorageService } from './../../../core/services/session-storage.service';
+import { LoginApiResponse } from './../../interfaces/login-api-response';
+import { SweetalertService } from './../../../core/services/sweetalert.service';
 import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthApiService } from '../../services/auth-api.service';
 
 @Component({
   selector: 'app-login-page',
@@ -13,13 +17,28 @@ export class LoginPageComponent implements OnInit {
     password: ['']
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private authApiService: AuthApiService, private sweetalert: SweetalertService,
+    private  sessionStorage: SessionStorageService) { }
 
   ngOnInit(): void {
   }
 
   onLoginSubmit(){
     console.log(this.loginForm.value);
+    const params = this.loginForm.value;
+    this.authApiService.login({
+      password: params.password,
+      email: params.mail
+    }).subscribe({
+      next: res =>{ 
+        this.sessionStorage.setItem("token", res.access_token);
+        
+    },
+      error : err => {
+        this.sweetalert.showApiErrors(err);
+      }
+  }
+    )
   }
 
 }
--- src/app/auth/services/auth-api.service.ts
+++ src/app/auth/services/auth-api.service.ts
@@ -1,9 +1,11 @@
+import { LoginApiResponse } from './../interfaces/login-api-response';
 import { RegisterApiResponse } from './../interfaces/register-api-response';
 import { RegisterApiRequest } from './../interfaces/register-api-request';
 import { PaddelLevelApiResponse } from './../interfaces/paddel-level-api-response';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LoginApiRequest } from '../interfaces/login-api-request';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +20,8 @@ export class AuthApiService {
 
   register(params : RegisterApiRequest):Observable<RegisterApiResponse>{
     return this.http.post<RegisterApiResponse>("http://padelpol.herokuapp.com/api/auth/register", params);
+  }
+  login(params : LoginApiRequest):Observable<LoginApiResponse>{
+    return this.http.post<LoginApiResponse>("http://padelpol.herokuapp.com/api/auth/login", params);
   }
 }
