16c708d2a7cfc878d4c4eecf06d5ec3a3e6b0f1e
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-16 14:24:25 +0100

    is authenticated guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { IsAuthenticatedGuard } from './core/guards/is-authenticated.guard';
 import { appRoutes } from './app-routes';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 { path: appRoutes.authModule, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, 
-{ path: appRoutes.gamesModule, loadChildren: () => import('./games/games.module').then(m => m.GamesModule) }
+{ path: appRoutes.gamesModule, loadChildren: () => import('./games/games.module').then(m => m.GamesModule), canActivate: [IsAuthenticatedGuard] }
 ];
 
 @NgModule({
