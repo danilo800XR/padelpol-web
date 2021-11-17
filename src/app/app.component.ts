d831bb230f3b60d786dce30e0839559dfa2c0e07
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-17 13:16:13 +0100

    hidde header & footer on auth module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { appRoutes } from './app-routes';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'padelpol-web';
+
+  authModulePath = appRoutes.authModule;
+
+  constructor(public router:Router){}
+  
 }
