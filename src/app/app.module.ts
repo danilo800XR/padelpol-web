9487c3fac8dddff6cde785a70cac1c515b48075b
Daniel GOMEZ FERNANDEZ <g_f_daniel@hotmail.com>
2021-11-17 12:45:00 +0100

    game page html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
